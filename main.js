26cdc9362b0c0c25311e915ea7c09f011350ec89
Eva Houžvičková <houzvickova@icloud.com>
2022-03-22 19:34:38 +0100

    ukoly hotove, for function inserted

--- main.js
+++ main.js
@@ -3,29 +3,29 @@
 
 
 // První příklad - vypiš vzestupně čísla od 0 do 10
-console.log('První příklad - vzestupná řada 0 - 10');
 
-//TADY NAPIS CYKLUS
-
-console.log('-------------------');
-
+for (let i = 0; i < 11; i = i + 1) {
+    console.log(i);
+}
 
 
 // Druhý příklad - zkopíruj předchozí příklad
 // a vyzkoušej, jaký je rozdíl mezi <= a <
 // v podmínce uvnitř cyklu
+console.log('druhý příklad jaký je rozdíl mezi <= a <');
 
-console.log('Druhý příklad - rozdíl mezi <= a <');
+for (let i = 0; i <= 11; i = i + 1) {
+    console.log(i);
+}
 
-//TADY NAPIS CYKLUS
-
-console.log('-------------------');
-
+for (let i = 0; i < 11; i = i + 1) {
+    console.log(i);
+}
 
 
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
 console.log('Třetí příklad - sestupná řada 10 - 0');
 
-//TADY NAPIS CYKLUS
-
-console.log('-------------------');
+for (let i = 10; i >= 0; i = i - 1) {
+    console.log(i);
+}
