b45c93aec930714689cf080b956a3f10f7eb7ac6
Eva Houžvičková <houzvickova@icloud.com>
2022-03-23 13:55:51 +0100

    ukoly prepsane pomoci while cyklu

--- main.js
+++ main.js
@@ -4,28 +4,29 @@
 
 // První příklad - vypiš vzestupně čísla od 0 do 10
 
-for (let i = 0; i < 11; i = i + 1) {
-    console.log(i);
-}
+let a = -1;
 
+while (a < 10) {
+    a = a + 1;
+    console.log(a);
+}
 
 // Druhý příklad - zkopíruj předchozí příklad
 // a vyzkoušej, jaký je rozdíl mezi <= a <
 // v podmínce uvnitř cyklu
 console.log('druhý příklad jaký je rozdíl mezi <= a <');
 
-for (let i = 0; i <= 11; i = i + 1) {
-    console.log(i);
-}
-
-for (let i = 0; i < 11; i = i + 1) {
-    console.log(i);
+while (a <= 10) {
+    a = a + 1;
+    console.log(a);
 }
 
 
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
 console.log('Třetí příklad - sestupná řada 10 - 0');
 
-for (let i = 10; i >= 0; i = i - 1) {
-    console.log(i);
+let b = 11;
+while (b > 0) {
+    b = b - 1
+    console.log(b);
 }
